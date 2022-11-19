f423ce6fa928585d0d76f3ff78501846d8190979
Pavel Andreev <pavel.andreev.dev@gmail.com>
2022-11-19 18:26:26 +0400

    logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Button } from './components/Button';
 import './App.css';
+import Logo from './Logo.svg';
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <img src={Logo} />
+      </header>
       <section className="App-content">
         <Button>Показать еще 5 билетов!</Button>
       </section>
